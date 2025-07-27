0dd24128ef68e18861c568869d49751f79d655e7
Daryon Dirk Ambasse <daryonambasse@gmail.com>
2025-07-27 13:15:25 -0700

    George got too curious again

---
+++ display.py
@@ -0,0 +1,103 @@
+
+from micropython import const
+import framebuf
+import time
+
+SET_CONTRAST = const(0x81)
+SET_ENTIRE_ON = const(0xA4)
+SET_NORM_INV = const(0xA6)
+SET_DISP = const(0xAE)
+SET_MEM_ADDR = const(0x20)
+SET_COL_ADDR = const(0x21)
+SET_PAGE_ADDR = const(0x22)
+SET_DISP_START_LINE = const(0x40)
+SET_SEG_REMAP = const(0xA0)
+SET_MUX_RATIO = const(0xA8)
+SET_IREF_SELECT = const(0xAD)
+SET_COM_OUT_DIR = const(0xC0)
+SET_DISP_OFFSET = const(0xD3)
+SET_COM_PIN_CFG = const(0xDA)
+SET_DISP_CLK_DIV = const(0xD5)
+SET_PRECHARGE = const(0xD9)
+SET_VCOM_DESEL = const(0xDB)
+SET_CHARGE_PUMP = const(0x8D)
+
+class SSD1306_DualSPI(framebuf.FrameBuffer):
+    def __init__(self, width, height, spi, dc, res, cs1, cs2, external_vcc=False):
+        self.width = width
+        self.height = height
+        self.external_vcc = external_vcc
+        self.pages = self.height // 8
+        self.buffer = bytearray(self.pages * self.width)
+        super().__init__(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
+
+        self.spi = spi
+        self.dc = dc
+        self.res = res
+        self.cs1 = cs1
+        self.cs2 = cs2
+        self.rate = 10 * 1024 * 1024
+
+        for pin in (dc, res, cs1, cs2):
+            pin.init(pin.OUT, value=0)
+
+        self.reset()
+        self.init_display()
+
+    def reset(self):
+        self.res(1)
+        time.sleep_ms(1)
+        self.res(0)
+        time.sleep_ms(10)
+        self.res(1)
+
+    def write_cmd(self, cmd, cs):
+        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        cs(1)
+        self.dc(0)
+        cs(0)
+        self.spi.write(bytearray([cmd]))
+        cs(1)
+
+    def write_data(self, buf, cs):
+        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        cs(1)
+        self.dc(1)
+        cs(0)
+        self.spi.write(buf)
+        cs(1)
+
+    def init_display(self):
+        for cs in (self.cs1, self.cs2):
+            for cmd in (
+                SET_DISP,
+                SET_MEM_ADDR, 0x00,
+                SET_DISP_START_LINE,
+                SET_SEG_REMAP | 0x01,
+                SET_MUX_RATIO, self.height - 1,
+                SET_COM_OUT_DIR | 0x08,
+                SET_DISP_OFFSET, 0x00,
+                SET_COM_PIN_CFG, 0x02 if self.width > 2 * self.height else 0x12,
+                SET_DISP_CLK_DIV, 0x80,
+                SET_PRECHARGE, 0x22 if self.external_vcc else 0xF1,
+                SET_VCOM_DESEL, 0x30,
+                SET_CONTRAST, 0xFF,
+                SET_ENTIRE_ON,
+                SET_NORM_INV,
+                SET_IREF_SELECT, 0x30,
+                SET_CHARGE_PUMP, 0x10 if self.external_vcc else 0x14,
+                SET_DISP | 0x01,
+            ):
+                self.write_cmd(cmd, cs)
+        self.fill(0)
+        self.show()
+
+    def show(self):
+        for page in range(self.pages):
+            left = page * 256
+            right = left + 128
+            for cs, start in ((self.cs1, left), (self.cs2, right)):
+                self.write_cmd(0xB0 | page, cs)
+                self.write_cmd(0x00, cs)
+                self.write_cmd(0x10, cs)
+                self.write_data(self.buffer[start:start + 128], cs)
--- main.py
+++ main.py
@@ -1,60 +1,87 @@
 
 from machine import Pin, SPI
-# Download the ssd1306.py driver to your project directory
-# You can use: wget https://raw.githubusercontent.com/micropython/micropython/master/drivers/display/ssd1306.py
+from display import SSD1306_DualSPI
+from radio import Radio
+import time
 
-from ssd1306 import SSD1306_SPI
-
-# SPI shared lines
-spi = SPI(0, baudrate=10_000_000, polarity=0, phase=0, sck=Pin(18), mosi=Pin(19))
-
-# Shared DC and RES pins
+# Setup SPI Display
+spi = SPI(0, sck=Pin(18), mosi=Pin(19))
 dc = Pin(20)
 res = Pin(21)
+cs1 = Pin(17)
+cs2 = Pin(5)
 
-# Individual CS pins
-cs1 = Pin(7)
-cs2 = Pin(22)
+display = SSD1306_DualSPI(256, 64, spi, dc, res, cs1, cs2)
 
-# Individual displays
-oled1 = SSD1306_SPI(128, 64, spi, dc, res, cs1)
-oled2 = SSD1306_SPI(128, 64, spi, dc, res, cs2)
+# Setup buttons
+btn_mode = Pin(0, Pin.IN, Pin.PULL_UP)
+btn_select = Pin(1, Pin.IN, Pin.PULL_UP)
+btn_up = Pin(2, Pin.IN, Pin.PULL_UP)
+btn_down = Pin(3, Pin.IN, Pin.PULL_UP)
 
-# Bridged Display class
-class BridgedOLED:
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
-        self.width = 256
-        self.height = 64
+last_mode = 1
+mode = 0
+alarm_time = [6, 30]  # 6:30 AM
+alarm_active = False
 
-    def fill(self, color):
-        self.left.fill(color)
-        self.right.fill(color)
+# Setup radio
+fm = Radio(101.9, 2, False)
 
-    def pixel(self, x, y, color):
-        if x < 128:
-            self.left.pixel(x, y, color)
+def draw_clock():
+    now = time.localtime()
+    display.text("Time: {:02d}:{:02d}".format(now[3], now[4]), 10, 10)
+    if alarm_active:
+        display.text("Alarm: {:02d}:{:02d}".format(*alarm_time), 140, 10)
+
+def draw_alarm_set():
+    display.text("Set Alarm:", 10, 10)
+    display.text("Hour: {:02d}".format(alarm_time[0]), 10, 30)
+    display.text("Min : {:02d}".format(alarm_time[1]), 140, 30)
+
+def draw_radio():
+    display.text("FM Radio", 10, 10)
+    display.text("Freq: {:.1f}".format(fm.Frequency), 10, 30)
+    display.text("Vol: {}".format(fm.Volume), 140, 30)
+
+while True:
+    # Mode button
+    current_mode = btn_mode.value()
+    if last_mode == 1 and current_mode == 0:
+        mode = (mode + 1) % 4
+        time.sleep(0.25)
+    last_mode = current_mode
+
+    display.fill(0)
+
+    if mode == 0:  # Clock
+        draw_clock()
+    elif mode == 1:  # Alarm Set
+        draw_alarm_set()
+        if not btn_up.value():
+            alarm_time[0] = (alarm_time[0] + 1) % 24
+            time.sleep(0.2)
+        if not btn_down.value():
+            alarm_time[1] = (alarm_time[1] + 1) % 60
+            time.sleep(0.2)
+        if not btn_select.value():
+            alarm_active = not alarm_active
+            time.sleep(0.2)
+    elif mode == 2:  # Radio
+        draw_radio()
+        if not btn_up.value():
+            fm.SetVolume((fm.Volume + 1) % 16)
+            fm.ProgramRadio()
+            time.sleep(0.2)
+        if not btn_down.value():
+            fm.SetVolume((fm.Volume - 1) % 16)
+            fm.ProgramRadio()
+            time.sleep(0.2)
+    elif mode == 3:  # Alarm check
+        now = time.localtime()
+        if alarm_active and (now[3], now[4]) == tuple(alarm_time):
+            display.text("WAKE UP!", 80, 25)
         else:
-            self.right.pixel(x - 128, y, color)
+            display.text("No Alarm Now", 80, 25)
 
-    def text(self, string, x, y, color=1):
-        for i, char in enumerate(string):
-            px = x + i * 8
-            if px + 8 <= 128:
-                self.left.text(char, px, y, color)
-            elif px >= 128:
-                self.right.text(char, px - 128, y, color)
-            else:
-                pass
-
-    def show(self):
-        self.left.show()
-        self.right.show()
-
-# Instantiate and test
-display = BridgedOLED(oled1, oled2)
-display.fill(0)
-display.text("LEFT SIDE", 10, 10)
-display.text("RIGHT SIDE", 140, 10)
-display.show()
+    display.show()
+    time.sleep(0.05)
